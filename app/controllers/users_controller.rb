c828fe7428bf9b7e40b2cb5b836d262c81d92d51
Shay Weiss <shay.weiss@algowiz.com>
2013-05-02 17:14:06 +0300

    Adding signout + tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      sign_in @user
+      sign_in @UsersController
       flash[:success] = "Welcome to the Shay's first Rails App!"
       redirect_to @user
     else

31b19427ac40519b58764296069dde3d758abaf0
Shay Weiss <shay.weiss@algowiz.com>
2013-05-02 17:32:58 +0300

    Fixed malicious bug!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      sign_in @UsersController
+      sign_in @user
       flash[:success] = "Welcome to the Shay's first Rails App!"
       redirect_to @user
     else
